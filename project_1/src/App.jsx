ec7944a222a50e89f4edf4348ee39c37cb379af4
NithinSRT <nithinp2606@gmail.com>
2023-12-19 12:38:22 +0530

    project1

---
+++ project_1/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './Components/nav'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <Nav /> */}
+      
+    </>
+  )
+}
+
+export default App
